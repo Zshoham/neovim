e325808f7665e9842195f7b43ee03de15ffe6ec8
Zshoham <ShohamZarfti@gmail.com>
2023-08-05 12:37:35 +0300

    updater: install lsp sepretly

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -17,4 +17,18 @@ return {
       }
     end,
   },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
 }
--- updater.py
+++ updater.py
@@ -1,13 +1,14 @@
-import subprocess
-from time import time
-import re
 import os
-from time import sleep
+import re
+import subprocess
 from shlex import split
+from time import sleep, time
 
 LAZY_COMMAND = "nvim --headless +\"lua require('lazy')\""
-MASON_LIST_COMMAND = "nvim --headless +\"lua print(vim.inspect(require('mason.settings').current.ensure_installed)); vim.api.nvim_command('quit')\""
-
+MASON_LIST_COMMAND = "nvim --headless +\"lua print(vim.inspect(require('mason.settings').current.ensure_installed)); vim.api.nvim_command('quit')\""  # noqa: E501
+INSTALL_LSP_COMMAND = (
+    "nvim --headless +\"lua require('mason-lspconfig.ensure_installed')()\""
+)
 proc = subprocess.Popen(
     split(LAZY_COMMAND), stderr=subprocess.PIPE, stdout=subprocess.PIPE
 )
@@ -53,14 +54,15 @@ proc_mason = subprocess.Popen(
 _, nvim_output = proc_mason.communicate(timeout=10)  # nvim sends output to stderr
 mason_plugins = set(re.findall(r'"(\w+)"', str(nvim_output)))
 
-home_dir = os.environ["HOME"]
-mason_log_file = f"{home_dir}/.local/state/nvim/mason.log"
+state_dir = os.environ["XDG_STATE_HOME"]
+mason_log_file = f"{state_dir}/nvim/mason.log"
 while not os.path.isfile(mason_log_file):
     sleep(1)
 
+
 installed_plugins = set()
 waiting_time = 0
-with open(f"{home_dir}/.local/state/nvim/mason.log", "r") as log_file:
+with open(f"{state_dir}/nvim/mason.log", "r") as log_file:
     while not mason_plugins.issubset(installed_plugins):
         new_lines = log_file.read()
         if not new_lines:
@@ -69,16 +71,43 @@ with open(f"{home_dir}/.local/state/nvim/mason.log", "r") as log_file:
             if waiting_time > 300:
                 raise RuntimeError("neovim update did not finish in time")
             continue
-        
+
         print(new_lines)
         mason_plugins.update(
-            re.findall(r"Executing installer for Package\(name=(.+)\)", new_lines)
+            re.findall(r"Executing installer for Package\(name=(\S+)\)", new_lines)
         )
         print(f"needed plugins are: {mason_plugins}")
         installed_plugins.update(
-            re.findall(r"Installation succeeded for Package\(name=(.+)\)", new_lines)
+            re.findall(r"Installation succeeded for Package\(name=(\S+)\)", new_lines)
         )
         print(f"installed plugins are: {installed_plugins}")
 
-sleep(1)
 proc.terminate()
+
+proc_install_lsp = subprocess.Popen(split(INSTALL_LSP_COMMAND), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+assert proc_install_lsp.stderr is not None
+install_lsp_output_fd = proc_install_lsp.stderr.fileno()
+
+to_install = set()
+installed = set()
+all_output = ""
+while not to_install or (not to_install.issubset(installed)):
+    new_text = os.read(install_lsp_output_fd, 256).decode('utf-8')
+    all_output += new_text
+    if not new_text:
+        sleep(0.1)
+        waiting_time += 0.1
+        if waiting_time > 300:
+            raise RuntimeError("neovim update did not finish in time")
+        continue
+
+    print(new_text)
+
+    to_install.update(re.findall(r" installing (\S+)\[", all_output))
+    print(f"to install: {to_install}")
+    installed.update(re.findall(r" (\S+) was successfully installed", all_output))
+    print(f"installed: {installed}")
+
+proc_install_lsp.terminate()
+
